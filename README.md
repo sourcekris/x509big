ea02c38edc648a6052441e12ed788e990d71cfc8
Kris Hunt <sourcekris@users.noreply.github.com>
2017-09-02 17:44:00 +1000

    Add support for BigPrivateKey type and support marshalling of BigPrivateKey type

--- README.md
+++ README.md
@@ -1,15 +1,19 @@
 ## x509big
 
-This package provides a partial fork of x509 with support for large integers in the exponent of RSA Public Keys.
+This package provides a partial fork of x509 with support for large integers in the public exponent of RSA Public Keys. It also provides the necessary support to marshal a Private Key with a large public exponent into a PKCS1 Private Key for encoding as ASN1
 
 This is useful for experimentation with unsual or known faulty RSA keys with public exponents larger than MaxUint64.
 
 ## Features
 
-`func ParseBigPKIXPublicKey(derBytes []byte) (pub interface{}, err error)`
+ * `func ParseBigPKIXPublicKey(derBytes []byte) (pub interface{}, err error)`
+   returns an interface you can assert to be a `BigPublicKey` type which is defined as
 
-returns an interface you can assert to be a `BigPublicKey` type which is defined as
+ * `func MarshalPKCS1BigPrivateKey(key *BigPrivateKey) []byte`
+   returns a array of bytes suitable to encode with something like `pem.EncodeToMemory()`
 
+
+## Types
 ```
 type BigPublicKey struct {
   N *big.Int
@@ -17,10 +21,24 @@ type BigPublicKey struct {
 }
 ```
 
-An example implementation:
+```
+type BigPrivateKey struct {
+        PublicKey BigPublicKey  // public part.
+        D         *big.Int      // private exponent
+        Primes    []*big.Int    // prime factors of N, has >= 2 elements.
+
+        // Precomputed contains precomputed values that speed up private
+        // operations, if available.
+        Precomputed PrecomputedValues
+}
+```
+
+## Examples
+
+An example implementation of parsing a RSA Public Key with a large public exponent
 
 ```
-func parsePublicRsaKey(keyBytes []byte) (*BigPublicKey, error) {
+func parsePublicRsaKey(keyBytes []byte) (*x509big.BigPublicKey, error) {
   key, err := x509big.ParseBigPKIXPublicKey(keyBytes)
   if err != nil {
     return nil, errors.New("Failed to parse the DER key after decoding.")
@@ -35,6 +53,26 @@ func parsePublicRsaKey(keyBytes []byte) (*BigPublicKey, error) {
   }
 }
 ```
+
+An example of marshalling a BigPrivateKey type into an PEM private key.
+
+```
+func encodeDerToPem(der []byte, t string) string {
+  p := pem.EncodeToMemory(
+    &pem.Block{
+      Type: t, 
+      Bytes: der,
+      },
+    )
+  return string(p)
+}
+
+func EncodeBigPrivateKey(priv *x509big.BigPrivateKey) string {
+  privder := x509big.MarshalPKCS1BigPrivateKey(priv)
+  fmt.Println(encodeDerToPem(privder, "RSA PRIVATE KEY"))
+}
+```
+
 ## Install
 
 Use go to install the library
--- x509big.go
+++ x509big.go
@@ -13,21 +13,78 @@ import (
   "math/big"
 )
 
+var bigOne = big.NewInt(1)
+
 type publicKeyInfo struct {
     Raw       asn1.RawContent
     Algorithm pkix.AlgorithmIdentifier
     PublicKey asn1.BitString
 }
 
-// an big public key type
+// a big public key type
 type BigPublicKey struct {
   N *big.Int
   E *big.Int
 }
 
+// a big private key type
+type BigPrivateKey struct {
+        PublicKey BigPublicKey  // public part.
+        D         *big.Int      // private exponent
+        Primes    []*big.Int    // prime factors of N, has >= 2 elements.
+
+        // Precomputed contains precomputed values that speed up private
+        // operations, if available.
+        Precomputed PrecomputedValues
+}
+
+// directly taken from standard library rsa.PrecomputedValues
+type PrecomputedValues struct {
+  Dp, Dq *big.Int // D mod (P-1) (or mod Q-1)
+  Qinv   *big.Int // Q^-1 mod P
+
+  // CRTValues is used for the 3rd and subsequent primes. Due to a
+  // historical accident, the CRT for the first two primes is handled
+  // differently in PKCS#1 and interoperability is sufficiently
+  // important that we mirror this.
+  CRTValues []CRTValue
+}
+
+// CRTValue contains the precomputed Chinese remainder theorem values.
+// directly taken from standard library rsa.CRTValues
+type CRTValue struct {
+  Exp   *big.Int // D mod (prime-1).
+  Coeff *big.Int // R·Coeff ≡ 1 mod Prime.
+  R     *big.Int // product of primes prior to this (inc p and q).
+}
+
+type pkcs1AdditionalRSAPrime struct {
+    Prime *big.Int
+  
+    // We ignore these values because rsa will calculate them.
+    Exp   *big.Int
+    Coeff *big.Int
+  }
+
+// pkcs1BigPrivateKey is a structure which mirrors the PKCS#1 ASN.1 for a RSA private key.
+type pkcs1BigPrivateKey struct {
+  Version int
+  N       *big.Int
+  E       *big.Int
+  D       *big.Int
+  P       *big.Int
+  Q       *big.Int
+  // We ignore these values, if present, because rsa will calculate them.
+  Dp   *big.Int `asn1:"optional"`
+  Dq   *big.Int `asn1:"optional"`
+  Qinv *big.Int `asn1:"optional"`
+
+  AdditionalPrimes []pkcs1AdditionalRSAPrime `asn1:"optional,omitempty"`
+}
+
 // ParseBigPKIXPublicKey parses a DER encoded public key. These values are
 // typically found in PEM blocks with "BEGIN PUBLIC KEY".
-// Taken from the standard library so we can return a gmp PublicKey
+// Taken from the standard library so we can return a big PublicKey
 func ParseBigPKIXPublicKey(derBytes []byte) (pub interface{}, err error) {
   var pki publicKeyInfo
   if rest, err := asn1.Unmarshal(derBytes, &pki); err != nil {
@@ -101,5 +158,69 @@ func parsePublicKey(algo x509.PublicKeyAlgorithm, keyData *publicKeyInfo) (inter
     return nil,errors.New("x509big: ECDSA Public Keys not supported")
   default:
     return nil, nil
+  }
+}
+
+// MarshalPKCS1PrivateKey converts a big private key to ASN.1 DER encoded form.
+// Taken from the standard library
+func MarshalPKCS1BigPrivateKey(key *BigPrivateKey) []byte {
+  key.precompute()
+
+  version := 0
+  if len(key.Primes) > 2 {
+    version = 1
+  }
+
+  priv := pkcs1BigPrivateKey{
+    Version: version,
+    N:       key.PublicKey.N,
+    E:       key.PublicKey.E,
+    D:       key.D,
+    P:       key.Primes[0],
+    Q:       key.Primes[1],
+    Dp:      key.Precomputed.Dp,
+    Dq:      key.Precomputed.Dq,
+    Qinv:    key.Precomputed.Qinv,
+  }
+
+  priv.AdditionalPrimes = make([]pkcs1AdditionalRSAPrime, len(key.Precomputed.CRTValues))
+  for i, values := range key.Precomputed.CRTValues {
+    priv.AdditionalPrimes[i].Prime = key.Primes[2+i]
+    priv.AdditionalPrimes[i].Exp = values.Exp
+    priv.AdditionalPrimes[i].Coeff = values.Coeff
+  }
+
+  b, _ := asn1.Marshal(priv)
+  return b
+}
+
+// Precompute performs some calculations that speed up private key operations
+// in the future. Taken from the standard library rsa.Precompute
+func (priv *BigPrivateKey) precompute() {
+  if priv.Precomputed.Dp != nil {
+    return
+  }
+
+  priv.Precomputed.Dp = new(big.Int).Sub(priv.Primes[0], bigOne)
+  priv.Precomputed.Dp.Mod(priv.D, priv.Precomputed.Dp)
+
+  priv.Precomputed.Dq = new(big.Int).Sub(priv.Primes[1], bigOne)
+  priv.Precomputed.Dq.Mod(priv.D, priv.Precomputed.Dq)
+
+  priv.Precomputed.Qinv = new(big.Int).ModInverse(priv.Primes[1], priv.Primes[0])
+
+  r := new(big.Int).Mul(priv.Primes[0], priv.Primes[1])
+  priv.Precomputed.CRTValues = make([]CRTValue, len(priv.Primes)-2)
+  for i := 2; i < len(priv.Primes); i++ {
+    prime := priv.Primes[i]
+    values := &priv.Precomputed.CRTValues[i-2]
+
+    values.Exp = new(big.Int).Sub(prime, bigOne)
+    values.Exp.Mod(priv.D, values.Exp)
+
+    values.R = new(big.Int).Set(r)
+    values.Coeff = new(big.Int).ModInverse(r, prime)
+
+    r.Mul(r, prime)
   }
 }
